247af14de19ad98b13c492545134e0cd209ba04c
le_bampre <a.aravitskiy@gmail.com>
2021-05-22 15:35:16 +0300

    added 2 testing methods for http package

--- lib/main.dart
+++ lib/main.dart
@@ -371,6 +371,7 @@ class _AppState extends State<App> {
         ],
       ),
       onPressed: httpGet,
+      onLongPress: httpPost,
     );
   }
 
@@ -446,8 +447,15 @@ class _AppState extends State<App> {
   }
 
 
-  httpGet () async{
+  httpGet () async {
     var response = await http.get(Uri.parse('https://json.flutter.su/echo'));
+    print("Response status: ${response.statusCode}");
+    print("Response body: ${response.body}");
+  }
+
+  httpPost () async {
+    String z = _currentCoordinates(_gpsIsActive);
+    var response = await http.post(Uri.parse('https://json.flutter.su/echo'), body: {'imei':'$_deviceIMEI','location':'$z',});
     print("Response status: ${response.statusCode}");
     print("Response body: ${response.body}");
   }
